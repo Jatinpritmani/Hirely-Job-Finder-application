c4b30561a2ba720c7b067283130c5330b0c43475
Jatinpritmani <Jatinpritmani3@gmail.com>
2025-02-05 23:16:22 +0530

    add common structure of api and user registration api

---
+++ Backend/utilities/constants/response-constant.constants.js
@@ -0,0 +1,17 @@
+module.exports = {
+    RESP_HJFA_MS: 'HJFA_MS',
+    RESP_ERROR_STATUS: 'error_status',
+    RESP_CODE: 'code',
+    RESP_MSG: 'message',
+    RESP_DATA: 'data',
+    
+    // Message Code
+    RESP_SOMETHING_WENT_WRONG: 'Something went wrong!',
+    RESP_SUCCESS_MSG: 'The request was fullfiled successfully.',
+    RESP_AUTHORIZED_USER: 'Authorized User!!.',
+    RESP_UNAUTHORIZED_USER: 'Unauthorized User!!.',
+    RESP_EMPTY_LIST_FOUND: 'Empty List Found.',
+
+    RESP_EMAIL_EXISTS:"Email already registered"
+
+}
