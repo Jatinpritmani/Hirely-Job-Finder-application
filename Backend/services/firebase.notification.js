3b43356ea94d9a989bdf75b5777e2e3b82c4ab58
Jatinpritmani <Jatinpritmani3@gmail.com>
2025-02-16 13:11:54 +0530

    change env file

--- Backend/services/firebase.notification.js
+++ Backend/services/firebase.notification.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(process.env.FIREBASE);
+// const serviceAccount = JSON.parse(process.env.FIREBASE);
+const serviceAccount = require("./firebase-admin.json"); 
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
