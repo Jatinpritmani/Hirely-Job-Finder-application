bf104fe914937f0628d4159e349f121075be011f
Jatinpritmani <Jatinpritmani3@gmail.com>
2025-02-25 23:02:24 +0530

    changes for file

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,10 @@ const app = express();
 const routes = require('./config/routes.js')
 const figlet = require('figlet');
 const {dbConnect} =require('./utilities/services/db.js')
-
+const cors = require('cors');
+const path = require('path')
+app.use(cors());
+app.use('/resumes', express.static(path.join(__dirname, 'resumes')))
 app.use(express.json({ limit: '300mb' }));
 
 app.use(express.urlencoded({ extended: true, limit: '150mb' }));
